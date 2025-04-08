bcb2e53860d21508de5915e0f772356a506f13b5
Liavarr <liav22sanchez@gmail.com>
2025-04-08 20:40:43 +0200

    Test IntelliJ

--- di-demo/src/main/java/guru/springframework/DiDemoApplication.java
+++ di-demo/src/main/java/guru/springframework/DiDemoApplication.java
@@ -12,12 +12,13 @@ public class DiDemoApplication {
 	public static void main(String[] args) {
 		// Guardamos el context para usarlo ahora
 		ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
-		
+
 		// Ahora preguntamos al context que me de un Bean llamado "myController" desde el context
 		MyController controller = (MyController) ctx.getBean("myController");
 		
 		// Ahora llamamos al metodo del objeto bean del context, como vemos no hemos instanciado un objeto controller como tal, lo ha dado el contexto.
 		controller.hello();
+
 	}
 
 }
